4b1e10f2bf5b54758d6555456ecad0ec0c9f2608
Anthony Di Franco <anthony@difranco.info>
2021-07-07 18:39:40 -0400

    Add bit distribution fitting and sampling.

--- src/BitTools.jl
+++ src/BitTools.jl
@@ -30,4 +30,80 @@ end
     return out
 end
 
+using Clustering
+import StatsBase: sample, Weights
+using Distributions
+
+export findClustering, sample, sampleWeights
+
+function hammingDistances(data :: BitMatrix)
+    (dim, len) = size(data)
+    r = zeros(UInt8, len, len)
+    for i in 1:len
+        for j in 1:i
+            r[i,j] = r[j,i] = sum(data[:,i] .!= data[:,j])
+        end
+    end
+    return r
+end
+
+@inline score(r, dists) = sum(silhouettes(r, dists))
+
+function tryNewClustering(k :: Int, oldClustering :: KmeansResult, data :: BitMatrix)
+    (dim, oldK) = size(oldClustering.centers)
+    if k == oldK
+        return oldClustering
+    end
+    c = copy(oldClustering.centers)
+
+    if k < oldK
+        c = c[:, 1:k]
+    elseif k > oldK
+        c = hcat(c, rand(dim, k - oldK))
+    end
+
+    return kmeans!(data, c, maxiter = 30)
+end
+
+function findClustering(data :: BitMatrix)
+    (dim, len) = size(data)
+    initialK = Int(max(floor(sqrt(sqrt(len))), 2))
+    initialGuess = kmeans(data, initialK, maxiter = 50)
+    dists = hammingDistances(data)
+
+    bestGuess = initialGuess
+    bestGuessScore = score(initialGuess, dists)
+
+    for k in initialK + 1 : Int(floor(1.5*sqrt(len)))
+        newGuess = tryNewClustering(k, bestGuess, data)
+        newGuessScore = score(newGuess, dists)
+
+        if(newGuessScore > bestGuessScore)
+            bestGuess = newGuess
+            bestGuessScore = newGuessScore
+        end
+    end
+
+    return bestGuess
+end
+
+function sampleWeights(c :: KmeansResult, numInSamples = 10)
+    idxs = collect(1:size(c.centers)[2])
+    points = sum(c.counts)
+    cols = sample(idxs, Weights(c.counts ./ points), numInSamples)
+    len = length(cols)
+    return sum(c.centers[:, cols], dims = 2) ./ len
+end
+
+function sample(c :: KmeansResult, numInSamples = 10, numOutSamples = 1)
+    weights = sampleWeights(c, numInSamples)
+    out = BitMatrix(undef, length(weights), numOutSamples)
+    for i in 1: length(weights)
+        for j in 1:numOutSamples
+            out[i, j] = rand(Bernoulli(weights[i]))
+        end
+    end
+    return out
+end
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -9,3 +9,19 @@ end
     b = BitVector([1, 0, 1, 0])
     @test invert_at_indices(b, [3,4]) == BitVector([1, 0, 0, 1])
 end
+
+@testset "bitfit estimate" begin
+    data = BitMatrix(rand(Bool, 3, 1000))
+    rightAnswer = Float64[1 1 1; 1 1 0; 1 0 1; 0 1 1; 0 0 0; 0 0 1; 0 1 0; 1 0 0]'
+    rightAnswer = sortslices(rightAnswer, dims = 2)
+    foundAnswer = sortslices(findClustering(data).centers, dims = 2)
+    @test sum(rightAnswer .- foundAnswer .|> abs) < 1e4
+end
+
+@testset "bitfit sample" begin
+    data = BitMatrix(rand(Bool, 3, 1000))
+    c = findClustering(data)
+    weights = sampleWeights(c, 10000)
+    answer = sum(sample(c, 10000, 100000), dims = 2) ./ 100000
+    @test sum(weights .- answer .|> abs) < 1e4
+end
